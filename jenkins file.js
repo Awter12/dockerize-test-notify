3284bcdb7884869b735515880eccaa1c051fe5c6
Awter12 <bretlegeju77@gmail.com>
2026-08-18 09:38:13 +0100

    slack edit

--- jenkins file.js
+++ jenkins file.js
@@ -37,10 +37,10 @@ pipeline {
     }
     post {
         success {
-            slackSend(channel: 'devops-1ra7213', color: 'good', message: "✅ Build succeeded: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
+            slackSend(channel: 'dockerize-test-notify', color: 'good', message: "✅ Build succeeded: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
         }
         failure {
-            slackSend(channel: 'devops-1ra7213', color: 'danger', message: "❌ Build failed: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
+            slackSend(channel: 'dockerize-test-notify', color: 'danger', message: "❌ Build failed: ${env.JOB_NAME} #${env.BUILD_NUMBER}")
         }
         always {
             sh 'docker logout'
