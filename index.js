3e3e6dcd43d4d3ba9c49256b65b9223442e8490e
Awter12 <bretlegeju77@gmail.com>
2026-08-18 11:20:25 +0100

    index file modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hey my name is mr bret . a lover of Devops" }');
 });
 
 app.get('/will', function (req, res) {
